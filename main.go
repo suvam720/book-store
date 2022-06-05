f7da06b8afb0fa4071eec9cf6eee8f0c0d02c839
suvam720 <debnath.suvamhhh@gmail.com>
2022-06-05 16:35:59 +0530

    project structure

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/joho/godotenv"
+	"github.com/suvam720/book-store/pkg/database"
 	"github.com/suvam720/book-store/pkg/routers"
 )
 
 func main() {
+	godotenv.Load(".env")
+	EnvKey, err := godotenv.Read(".env")
+	if err != nil {
+		log.Fatal("failed to read env file")
+	}
+	dsn := EnvKey["DB_STRING"]
+	err1 := database.Connect(dsn)
+	if err1 != nil {
+		log.Fatal(err)
+	}
 	routes := routers.Routes()
-	fmt.Println("Server started on port 8080...")
-	routes.Run(":8080")
+	routes.Run()
 }
