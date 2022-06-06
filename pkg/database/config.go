ffd7ad4af508ca644ec99923cdf3145463410df8
suvam720 <debnath.suvamhhh@gmail.com>
2022-06-06 14:25:56 +0530

    removed unnecessary comments

--- pkg/database/config.go
+++ pkg/database/config.go
@@ -1,11 +1,8 @@
 package database
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-
 	"github.com/suvam720/book-store/pkg/models"
 	"gorm.io/driver/mysql"
-	_ "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -22,30 +19,3 @@ func Connect(dsn string) error {
 
 	return nil
 }
-
-//var db *gorm.DB
-
-// func Connect() {
-// 	EnvKey, _ := godotenv.Read(".env")
-// 	dsn := EnvKey["DB_STRING"]
-// 	fmt.Printf("dsn: %v\n", dsn)
-// 	d, err := gorm.Open(mysql.Open(dsn))
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		panic("failed to connect database")
-// 	}
-// 	fmt.Printf("d:%v", d)
-// 	db = d
-// }
-
-// func GetDB() *gorm.DB {
-// 	return db
-// }
-
-// var Db *gorm.DB
-
-// func Connection() {
-// 	Connect()
-// 	Db = GetDB()
-// 	Db.AutoMigrate(&models.Book{})
-// }
